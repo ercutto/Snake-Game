2dcf6a11606ca9708e0608f207db788776002596
ercutto <ercumentti81@gmail.com>
2022-07-08 14:32:51 +0300

    Simple Snake game ready

--- snake.js
+++ snake.js
@@ -1,72 +1,108 @@
-//board
-var blockSize=25;
-var rows=20;
-var cols=20;
-var board;
-var context;
-//snakes head
-var snakeX=blockSize * 5;
-var snakeY=blockSize * 5;
-//snakes speed;
-var velocityX=0;
-var velocityY=0;
 
-//foods
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context; 
+
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+
+//food
 var foodX;
 var foodY;
 
-window.onload=function(){
-    board=document.getElementById("board");
-    board.height=rows * blockSize;
-    board.width=cols * blockSize;
-    context=board.getContext("2d");//draws board
-    spawnFood();
-    document.addEventListener("keyup",ChangeDirection);
-    //update();
-    setInterval(update,1000/10)//update tie setting
-}
-function update(){
-    context.fillStyle="black";
-    context.fillRect(0,0,board.width,board.height);
+var gameOver = false;
 
-    //food
-    context.fillStyle="yellow";
-    context.fillRect(foodX,foodY,blockSize,blockSize);
+window.onload = function() {
+    board = document.getElementById("board");
+
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    spawnFood();
+    document.addEventListener("keyup", changeDirection);
+    // update();
+    setInterval(update, 2500/10); //100 milliseconds
+}
+
+function update() {
+    if (gameOver) {
+        return;
+    }
     
-    //Snake and food colliding
-    if(snakeX == foodX && snakeY == foodY){
+    //background
+    context.fillStyle="gray";
+    context.fillRect(0, 0, board.width, board.height);
+    //food
+    context.fillStyle="pink";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+    //collision
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
         spawnFood();
     }
 
-    //snake
-    context.fillStyle="crimson";
-
-    //Snakes movement
-    snakeX+=velocityX*blockSize;
-    snakeY+=velocityY*blockSize;
-    context.fillRect(snakeX,snakeY,blockSize,blockSize);
-
-   
-
-
-}
-function ChangeDirection(e){
-    if(e.code=="ArrowUp"){
-        velocityX=0;
-        velocityY=-1;
-    } else if(e.code=="ArrowDown"){
-        velocityX=0;
-        velocityY=1;
-    } else if(e.code=="ArrowLeft"){
-        velocityX=-1;
-        velocityY=0;
-    } else if(e.code=="ArrowRight"){
-        velocityX=1;
-        velocityY=0;
+    for (let i = snakeBody.length-1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+    //snake and movement
+    context.fillStyle="aquamarine";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    //grooves
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
     }
 
+    //game over conditions
+    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
 }
-function spawnFood(){
-    foodX=math.floor(Math.random()*cols)*blockSize;
-    foodY=math.floor(Math.random()*rows)*blockSize;
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+
+function spawnFood() {
+    //(0-1) * cols -> (0-19.9999) -> (0-19) * 25
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
